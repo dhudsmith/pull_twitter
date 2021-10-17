d8c5b68d253686f665808acd86e4f9859db12ee6
NickDeas <nickid0419@gmail.com>
2021-10-17 15:50:48 -0400

    Referenced tweets and authors expansion support for Tweet Search

--- utils/tweet_search.py
+++ utils/tweet_search.py
@@ -46,7 +46,7 @@ class TweetSearch:
 		print(f"Pulling tweet results using '{query}' search query.")
 
 		# setup save directory
-		save_path = f"{output_dir}/data.{save_format}"
+		save_path = f"{output_dir}/data_%s.{save_format}"
 		print(f"Saving tweets to {save_path}")
 
 		num_batches = (max_results//batch_size) + 1
@@ -63,7 +63,7 @@ class TweetSearch:
 
 		next_token = None
 		num_collected = 0
-		df_tweets = None
+		df_links, df_refs, df_users, df_tweets, df_media = None, None, None, None, None
 		for batch in batches:
 			
 			# Get tweet data from twitter api
@@ -81,15 +81,71 @@ class TweetSearch:
 
 			# insert tweets into file
 			tweets: List[dict] = response.data
+			has_refs: bool = 'referenced_tweets' in self.query_params.tweet_fields
+			inc_tweets: List[dict] = response.includes['tweets'] if 'tweets' in response.includes.keys() else None
+			inc_users: List[dict] = response.includes['users'] if 'users' in response.includes.keys() else None
+			# inc_media: List[dict] = response.includes['media'] if 'media' in response.includes.keys() else None
+
 			if tweets:
+
+				# Expansions parsing
+				if inc_tweets:
+					ref_tweets = [twalc.Tweet(**tw) for tw in inc_tweets]
+				if inc_users:
+					authors = [twalc.User(**us) for us in inc_users]
+				# if inc_media:
+				#     media = [twalc.Media(**md) for md in inc_media]
+				if has_refs:
+					links = TweetSearch.__parse_tweet_links(tweets)
+
+				# Original Tweets Parsing
 				tweets = [twalc.Tweet(**tw).to_dict() for tw in tweets]
 
+				# Expansions dataframes
+				if inc_tweets:
+					df_refs   = pd.concat([df_refs, pd.DataFrame(ref_tweets)], axis = 1) if df_refs is not None else pd.DataFrame(ref_tweets)
+				if inc_users:
+					df_users  = pd.concat([df_users, pd.DataFrame(authors)], axis = 1) if df_users is not None else pd.DataFrame(authors)
+				# if inc_media:
+				#     df_media  = pd.concat([df_media, pd.DataFrame(media)], axis = 1) if df_media is not None else pd.DataFrame(media)
+				if has_refs:
+					df_links  = pd.concat([df_links, pd.DataFrame(links)], axis = 1) if df_links is not None else pd.DataFrame(links)
+
+				# Original tweets dataframe
 				df_tweets = pd.concat([df_tweets, pd.DataFrame(tweets)], axis = 1) if df_tweets is not None else pd.DataFrame(tweets)
 
 				if save_format == 'csv':
-					df_tweets.to_csv(save_path, index=False, quoting=csv.QUOTE_ALL,
+					# Expansions saving
+					# Full referenced tweets data
+					if inc_tweets:
+						df_refs.to_csv(save_path % 'ref_tweets', index=False, quoting=csv.QUOTE_ALL,
 										header=True)
+					# Full author user data
+					if inc_users:
+						df_users.to_csv(save_path % 'authors', index=False, quoting=csv.QUOTE_ALL,
+										header=True)
+					# Full media data
+					# if inc_media:
+					#     df_media.to_csv(save_path % 'media', index=False, quoting=csv.QUOTE_ALL,
+					#                     header=True)
+					# parent-child links for referenced_tweets
+					if has_refs:
+						df_links.to_csv(save_path % 'ref_links', index=False, quoting = csv.QUOTE_ALL,
+										header=True)
+
+					# Original Tweets Saving
+					df_tweets.to_csv(save_path % 'tweets', index=False, quoting=csv.QUOTE_ALL,
+									header=True)
+
 				elif save_format == 'json':
+					if inc_tweets:
+						df_refs.to_json(save_path % 'ref_tweets', orient = 'table')
+					if inc_users:
+						df_users.to_json(save_path % 'authors', orient = 'table')
+					# if inc_media:
+					    # df_media.to_json(save_path % 'media', orient = 'table')
+					if has_refs:
+						df_links.to_json(save_path % 'ref_links', orient = 'table')
 					df_tweets.to_json(save_path, orient = 'table')
 
 				num_collected += len(tweets)
@@ -104,6 +160,8 @@ class TweetSearch:
 			# Avoiding 1 request/sec rate limit
 			end_time_req = time.time()
 			time.sleep(max(0, 1.0 - (end_time_req-start_time_req)))
+
+
 
 
 	def search_tweets(self, query: str, 
@@ -137,3 +195,17 @@ class TweetSearch:
 				print("Sleeping for 0.1 seconds and retrying")
 				retries += 1
 				time.sleep(0.1)
+
+	@staticmethod
+	def __parse_tweet_links(tweets: List[Tweet]) -> List[dict]:
+		tweet_links = []
+		for tweet in tweets:
+			if tweet['referenced_tweets'] is not None:
+				for ref in tweet['referenced_tweets']:
+					new_link = {
+						'parent_id': tweet['id'], 
+						'id': ref['id'],
+						'type': ref['type']
+					}
+					tweet_links.append(new_link)
+		return tweet_links
